b9161f8f4b0cf3d85d8253e6e7457df0b8c2d3cc
2021-DEV1-002@bnp.org <alixamou1@gmail.com>
2021-08-30 10:25:27 +0200

    Initial commit for the kata

---
+++ src/test/java/com/bnp/kata/DEV1085/ApplicationTests.java
@@ -0,0 +1,25 @@
+package com.bnp.kata.DEV1085;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void isGameContinuing() {
+	}
+
+	@Test
+	void isGridFullWithoutWinner() {
+	}
+
+	@Test
+	void hasPlayerWon()
+	{
+	}
+}
