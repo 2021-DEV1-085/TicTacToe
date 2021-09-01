e07667699c514804ceaab3f593c1ff8587f1026d
2021-DEV1-002@bnp.org <alixamou1@gmail.com>
2021-09-01 10:30:20 +0200

    Output messages removed New endpoint for reset the game Message for the winner or no winner Grid cells have a class Cell for the style

--- src/main/java/com/bnp/kata/DEV1085/controller/GameController.java
+++ src/main/java/com/bnp/kata/DEV1085/controller/GameController.java
@@ -3,11 +3,10 @@ package com.bnp.kata.DEV1085.controller;
 import com.bnp.kata.DEV1085.object.Cell;
 import com.bnp.kata.DEV1085.service.GameService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.logging.Logger;
 
 @Controller
 public class GameController {
@@ -19,7 +18,7 @@ public class GameController {
 
     private Cell[][] cells;
     private static ROUND round = ROUND.PLAYER_ONE;
-    private static Logger logger = Logger.getLogger("GameController");
+    private String message;
 
     GameController() {
         this.initCells();
@@ -31,23 +30,20 @@ public class GameController {
 
     @RequestMapping(value = "/inGame")
     public String play(@ModelAttribute("selectedCell") Cell selectedCell) {
+        this.message = null;
         if (selectedCell != null) {
             Integer x = selectedCell.getX();
             Integer y = selectedCell.getY();
             if (round == ROUND.PLAYER_ONE) {
                 this.cells[x][y].setCharacter('x');
                 if (GameService.hasPlayerWon(cells, 'x'))
-                {
-                    System.out.println("Player 1 won");
-                }
+                    this.message = "Player 1 won";
                 round = ROUND.PLAYER_TWO;
             } else {
                 selectedCell.setCharacter('o');
                 this.cells[x][y].setCharacter('o');
                 if (GameService.hasPlayerWon(cells, 'o'))
-                {
-                    System.out.println("Player 2 won");
-                }
+                    this.message = "Player 2 won";
                 round = ROUND.PLAYER_ONE;
             }
         }
@@ -55,8 +51,18 @@ public class GameController {
     }
 
     @GetMapping(value = "/play")
-    public String beginGame() {
+    public String beginGame(Model model) {
+        if (this.message == null && GameService.isGridFull(this.cells))
+            this.message = "No winner is this game";
+        model.addAttribute("message", this.message);
         return "play";
+    }
+
+    @GetMapping(value = "/reset")
+    public String reset() {
+        this.initCells();
+        this.message = null;
+        return "redirect:/play";
     }
 
     @ModelAttribute("firstLine")
